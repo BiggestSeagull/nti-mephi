816879f1d9d873bc92eb902196cb77a6703be9e8
BiggestSeagull <biggestseagull@gmail.com>
2025-06-23 21:07:14 +0500

    Стили и скрипты переехали

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// Навигация
+document.querySelectorAll('.nav-line li').forEach(function (item) {
+    item.addEventListener('mouseenter', function () {
+        const submenu = this.querySelector('ul');
+        if (submenu) {
+            submenu.style.display = 'block';
+
+            // Сбрасываем стиль перед вычислениями
+            submenu.style.left = '0';
+            submenu.style.right = 'auto';
+
+            // Получаем позицию и размеры подменю и родительского элемента
+            const rect = submenu.getBoundingClientRect();
+
+            // Проверяем, выходит ли подменю за правую границу
+            if (rect.right > window.innerWidth) {
+                submenu.style.left = 'auto';
+                submenu.style.right = '0';
+            }
+        }
+    });
+
+    item.addEventListener('mouseleave', function () {
+        const submenu = this.querySelector('ul');
+        if (submenu) {
+            submenu.style.display = 'none';
+        }
+    });
+});
+
+
+// Дублирование слайдов
+const slidesContainer = document.querySelector(".news-slides");
+if (slidesContainer) {
+    slidesContainer.innerHTML += slidesContainer.innerHTML;
+}
+
+
+// Полезные ссылки
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdownContent = document.getElementById('dropdownContent');
+let isDropdownOpen = false;
+// Обработчик клика по кнопке
+dropdownButton.addEventListener('click', function (e) {
+    e.stopPropagation();
+    isDropdownOpen = !isDropdownOpen;
+
+    if (isDropdownOpen) {
+        dropdownContent.classList.add('active');
+    } else {
+        dropdownContent.classList.remove('active');
+    }
+});
+// Закрытие при клике вне меню
+document.addEventListener('click', function () {
+    if (isDropdownOpen) {
+        dropdownContent.classList.remove('active');
+        isDropdownOpen = false;
+    }
+});
+// Предотвращаем закрытие при клике внутри меню
+dropdownContent.addEventListener('click', function (e) {
+    e.stopPropagation();
+});
